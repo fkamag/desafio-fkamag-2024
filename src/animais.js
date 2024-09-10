f26f171fc1b0ff47cfe310ec56b454075fd23fe8
fkamag <fabiokamagian@gmail.com>
2024-09-09 21:33:01 -0300

    feat animais

---
+++ src/animais.js
@@ -0,0 +1,10 @@
+const animais = {
+    'LEAO': { tamanho: 3, biomas: ['savana'], carnivoro: true },
+    'LEOPARDO': { tamanho: 2, biomas: ['savana'], carnivoro: true },
+    'CROCODILO': { tamanho: 3, biomas: ['rio'], carnivoro: true },
+    'MACACO': { tamanho: 1, biomas: ['savana', 'floresta'], carnivoro: false },
+    'GAZELA': { tamanho: 2, biomas: ['savana'], carnivoro: false },
+    'HIPOPOTAMO': { tamanho: 4, biomas: ['savana', 'rio'], carnivoro: false },
+};
+
+export { animais };
