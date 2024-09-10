e8e573dfd57e1a6ec306d45684283c1117f896bd
fkamag <fabiokamagian@gmail.com>
2024-09-09 22:00:11 -0300

    feat verifica animal válido

--- src/recintos-zoo.js
+++ src/recintos-zoo.js
@@ -1,6 +1,18 @@
+import { recintos } from './recintos.js';
+import { animais } from './animais.js';
+
 class RecintosZoo {
 
+    constructor() {
+        this.recintos = recintos;
+        this.animais = animais;
+    }
+
     analisaRecintos(animal, quantidade) {
+
+        if (!(animal in animais)) {
+            return { erro: 'Animal inválido' };
+        }
     }
 
 }
